929db61995475d307394707604ca41e257ccabd6
Pannaga <pannaga1123@gmail.com>
2026-02-16 22:08:18 +0530

    update Quest.Heap.E2

--- LeetCode/Quest/Heap/E2. FindKPairsWithSmallestSums/index.js
+++ LeetCode/Quest/Heap/E2. FindKPairsWithSmallestSums/index.js
@@ -4,7 +4,7 @@
  * @param {number} k
  * @return {number[][]}
  */
-let kSmallestPairs = function(nums1, nums2, k) {
+let kSmallestPairs = function (nums1, nums2, k) {
     let result = [];
     let i = 0;
     let j = 0;
@@ -12,15 +12,25 @@ let kSmallestPairs = function(nums1, nums2, k) {
     let visitedPairs = new Set();
 
     const heapifyUp = (heap, i) => {
-        if(heap.length == 1) return;
+        if (heap.length == 1) return;
 
-        while(i > 0) {
+        while (i > 0) {
             let parent = Math.floor((i - 1) / 2);
             let parentSum = nums1[heap[parent][0]] + nums2[heap[parent][1]];
             let iSum = nums1[heap[i][0]] + nums2[heap[i][1]]
-            console.log("parent", nums1[heap[parent][0]] + nums2[heap[parent][1]])
-            if(parentSum <= iSum) break;
+            let needSwap = false;
 
+            if (parentSum > iSum) {
+                needSwap = true;
+            } else if (parentSum === iSum) {
+                if (heap[parent][0] > heap[i][0]) {
+                    needSwap = true;
+                } else if (heap[parent][0] === heap[i][0] && heap[parent][1] > heap[i][1]) {
+                    needSwap = true;
+                }
+            }
+
+            if (!needSwap) break;
             [heap[parent], heap[i]] = [heap[i], heap[parent]];
             i = parent;
         }
@@ -29,24 +39,42 @@ let kSmallestPairs = function(nums1, nums2, k) {
     }
 
     const heapifyDown = (heap, i, n) => {
-        if(!heap.length || heap.length == 1) return;
+        if (!heap.length || heap.length == 1) return;
 
-        while(true) {
+        while (true) {
             let smallest = i;
+            let smallestI = heap[smallest][0]
+            let smallestJ = heap[smallest][1]
             let smallestSum = nums1[heap[smallest][0]] + nums2[heap[smallest][1]]
             let left = 2 * i + 1;
             let leftSum = null;
-            if(heap[left]) leftSum = nums1[heap[left][0]] + nums2[heap[left][1]]
-            
+            if (heap[left]) leftSum = nums1[heap[left][0]] + nums2[heap[left][1]]
+
             let right = 2 * i + 2;
             let rightSum = null;
-            if(heap[right]) rightSum = nums1[heap[right][0]] + nums2[heap[right][1]]
-            console.log("i", i, "right", right, heap.length, heap)
+            if (heap[right]) rightSum = nums1[heap[right][0]] + nums2[heap[right][1]]
 
-            if(leftSum && leftSum < smallestSum) smallest = left
-            if(rightSum && rightSum < smallestSum) smallest = right
+            if (left < n && leftSum <= smallestSum) {
+                if (leftSum < smallestSum || heap[left][0] < smallestI ||
+                    (heap[left][0] === smallestI && heap[left][1] < smallestJ)) {
+                    smallest = left;
+                    smallestSum = leftSum;  // ADD THIS
+                    smallestI = heap[left][0];  // ADD THIS
+                    smallestJ = heap[left][1];  // ADD THIS
+                }
+            }
 
-            if(smallest == i) break;
+            if (right < n && rightSum <= smallestSum) {
+                if (rightSum < smallestSum || heap[right][0] < smallestI ||
+                    (heap[right][0] === smallestI && heap[right][1] < smallestJ)) {
+                    smallest = right;
+                    smallestSum = rightSum;  // ADD THIS
+                    smallestI = heap[right][0];  // ADD THIS
+                    smallestJ = heap[right][1];  // ADD THIS
+                }
+            }
+
+            if (smallest == i) break;
             [heap[i], heap[smallest]] = [heap[smallest], heap[i]]
             i = smallest;
         }
@@ -56,14 +84,19 @@ let kSmallestPairs = function(nums1, nums2, k) {
 
     const extractMinAndHeapify = () => {
         let min = heap[0];
-        console.log("LN.54", heap);
-        if(heap.length == 1) heap.pop();
-        else {
+        if (heap.length == 1) heap.pop();
+        else if (heap.length > 1) {
             heap[0] = heap.pop();
-            console.log("LN.58", heap);
             heapifyDown(heap, 0, heap.length);
+            for (let i = 0; i < heap.length; i++) {
+                let left = 2 * i + 1, right = 2 * i + 2;
+                if (left < heap.length) {
+                    let sumParent = nums1[heap[i][0]] + nums2[heap[i][1]];
+                    let sumLeft = nums1[heap[left][0]] + nums2[heap[left][1]];
+                    if (sumParent > sumLeft) console.log("VIOLATION at", i, "parent", sumParent, "left", sumLeft);
+                }
+            }
         }
-        console.log("LN.61", heap);
         return min
     }
 
@@ -72,23 +105,28 @@ let kSmallestPairs = function(nums1, nums2, k) {
         heapifyUp(heap, heap.length - 1);
         return heap;
     }
-    
-    while(result.length < k) {
-        console.log("Before Extracting", heap)
-        let [i, j] = extractMinAndHeapify();
-        console.log([i, j])
-        result.push([nums1[i], nums2[j]])
-        visitedPairs.add([i, j]);
 
-        if(!visitedPairs.has([i+1, j]) && nums1[i+1] && nums2[j]) insertIntoHeap([i+1, j])
-        if(!visitedPairs.has([i, j+1]) && nums1[i] && nums2[j+1]) insertIntoHeap([i, j+1])
+    while (result.length < k) {
+        console.log("Heap before extract:", heap.map(([i, j]) => `[${i},${j}]=${nums1[i] + nums2[j]}`).join(", "));
+        let [i, j] = extractMinAndHeapify();
+        if (visitedPairs.has(`${i}_${j}`)) {
+            console.log("Extracted:", i, j, "already visited: true - SKIPPING");
+            continue;
+        }
+        console.log("Extracted:", i, j, "already visited:", visitedPairs.has(`${i}_${j}`))
+        result.push([nums1[i], nums2[j]])
+        visitedPairs.add(`${i}_${j}`);
+        if (nums1[i] == 0 && nums2[j] == 22) console.log("Current Heap", heap)
+
+        if (!visitedPairs.has(`${i + 1}_${j}`) && i + 1 < nums1.length && j < nums2.length) insertIntoHeap([i + 1, j])
+        if (!visitedPairs.has(`${i}_${j + 1}`) && i < nums1.length && j + 1 < nums2.length) insertIntoHeap([i, j + 1])
 
     }
-    
+
     console.log("result", result)
     return result;
 };
 
 // kSmallestPairs([1, 1, 2], [1, 2, 3], 2);
 // kSmallestPairs([1,7,11], [2,4,6], 3);
-kSmallestPairs([0,0,0,0,0,2,2,2,2], [-3,22,35,56,76], 22);
+kSmallestPairs([0, 0, 0, 0, 0, 2, 2, 2, 2], [-3, 22, 35, 56, 76], 22);
