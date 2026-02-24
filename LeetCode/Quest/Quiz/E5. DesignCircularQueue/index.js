f99ea45887de67b2b6f7f2c3c28a7f5fb999fdf3
Pannaga <pannaga1123@gmail.com>
2026-02-25 00:37:06 +0530

    add Quest.Quiz.E5

---
+++ LeetCode/Quest/Quiz/E5. DesignCircularQueue/index.js
@@ -0,0 +1,82 @@
+/**
+ * @param {number} k
+ */
+let MyCircularQueue = function(k) {
+    this.front = 0;
+    this.rear = 0;
+    this.queue = new Array(k);
+    this.capacity = k;
+    this.itemsCount = 0;
+};
+
+/** 
+ * @param {number} value
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.enQueue = function(value) {
+    if(this.isFull()) return false;
+    
+    if(this.queue.length) {
+        this.queue[this.rear] = value;
+        this.rear = (this.rear + 1) % this.capacity;
+        this.itemsCount++
+    }
+    return true;
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.deQueue = function() {
+    if(this.isEmpty()) return false;
+    
+    if(this.queue.length) {
+        this.front = (this.front + 1) % this.capacity;
+        this.itemsCount--
+        return true;
+    }
+
+    return false
+    
+};
+
+/**
+ * @return {number}
+ */
+MyCircularQueue.prototype.Front = function() {
+    if(this.isEmpty()) return -1
+    return this.queue[this.front]
+};
+
+/**
+ * @return {number}
+ */
+MyCircularQueue.prototype.Rear = function() {
+    if(this.isEmpty()) return -1
+    return this.queue[(this.rear - 1 + this.capacity) % this.capacity]
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.isEmpty = function() {
+    return this.itemsCount == 0
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.isFull = function() {
+    return this.itemsCount == this.capacity
+};
+
+/** 
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * var obj = new MyCircularQueue(k)
+ * var param_1 = obj.enQueue(value)
+ * var param_2 = obj.deQueue()
+ * var param_3 = obj.Front()
+ * var param_4 = obj.Rear()
+ * var param_5 = obj.isEmpty()
+ * var param_6 = obj.isFull()
+ */
