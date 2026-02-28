77236a9793a5f4cf2038c51dd9a45d1e3b07f2f7
Pannaga <pannaga1123@gmail.com>
2026-02-28 23:08:48 +0530

    update LinkedList CRUD

--- DataStructures/Types/LinkedList/index.ts
+++ DataStructures/Types/LinkedList/index.ts
@@ -15,11 +15,87 @@ class LinkedList {
         this.head = null;
     }
 
-    append(value: number) {}
-    prepend(value: number) {}
-    find(value: number) {}
-    update(oldValue: number, newValue: number) {}
-    delete(value: number) {}
-    toArray() {}
-    size() {}
+    append(value: number) {
+        try {
+            const node = new ListNode(value, null);
+            if (!this.head) {
+                this.head = node;
+                return;
+            }
+
+            let current = this.head;
+
+            while (current.next) {
+                current = current.next;
+            }
+
+            current.next = node;
+            return true;
+        } catch (e: any) {
+            console.log("Fatal - append", e);
+            return false;
+        }
+    }
+
+    prepend(value: number) {
+        try {
+            let current = new ListNode(value, null);
+            if (!this.head) {
+                this.head = current;
+                return true;
+            }
+
+            current.next = this.head;
+            this.head = current;
+
+            return true;
+        } catch (e: any) {
+            console.log("Fatal - prepend", e);
+            return false;
+        }
+    }
+
+    find(value: number) {
+        try {
+            let current = this.head;
+            while(current?.next) {
+                if(current.val == value) return current
+
+                current = current.next
+            }
+
+            return null;
+        } catch (e: any) {
+            console.log("Fatal - find", e);
+            return null;
+        }
+    }
+
+    update(oldValue: number, newValue: number) {
+        try {
+            let node = this.find(oldValue);
+            if(node) {
+                node.val = newValue;
+                return true;
+            }
+
+            return null;
+        } catch (e: any) {
+            console.log("Fatal - update", e);
+            return null;
+        }
+    }
+
+    delete(value: number) {
+        try {
+            let current = this.head;
+            if(current)
+        } catch (e: any) {
+            console.log("Fatal - delete", e);
+            return null;
+        }
+    }
+
+    toArray() { }
+    size() { }
 }
