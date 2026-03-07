275b49597bb559f31919fcf9578505aad73aeccd
Pannaga <pannaga1123@gmail.com>
2026-03-07 23:06:58 +0530

    add Quest.PrefixSum.P3

---
+++ LeetCode/Quest/PrefixSum/P3. MinimumValueToGertPositiveStepByStepSum/index.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+let minStartValue = function(nums) {
+    let startValue = 1;
+    let prefixSum = startValue;
+    let i = 0;
+
+    while(true) {
+        if(nums[i] + prefixSum < 1) {
+            startValue++;
+            prefixSum = startValue;
+            i = 0;
+            continue;
+        }
+        
+        prefixSum += nums[i]
+        i++
+        if(i == nums.length - 1 && nums[i] + prefixSum > 1) return startValue;
+    }
+};
