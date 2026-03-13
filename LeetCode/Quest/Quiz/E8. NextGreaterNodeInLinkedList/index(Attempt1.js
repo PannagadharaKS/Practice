40b37a953a79e152bf6e5917a85bbc5ca9d3b56d
Pannaga <pannaga1123@gmail.com>
2026-03-13 23:33:20 +0530

    update Quest.Quiz.E8

--- LeetCode/Quest/Quiz/E8. NextGreaterNodeInLinkedList/index(Attempt1.js
+++ LeetCode/Quest/Quiz/E8. NextGreaterNodeInLinkedList/index(Attempt1.js
@@ -13,19 +13,20 @@ let nextLargerNodes = function(head) {
     let current = head;
     let greater = head;
     let output = [];
+    let linkedListToArray = [];
 
-    while(current != null && current.next != null) {
-        if(greater == null || greater.next == null) {
+    while(current != null) {
+        if(greater == null) {
             output.push(0)
             current = current.next;
+            greater = current;
             continue
         }
-
-        console.log("current", current.value)
-        console.log("greater", greater.value)
-        if(greater.value > current.value) {
-            output.push(greater.value)
+        
+        if(greater.val > current.val) {
+            output.push(greater.val)
             current = current.next;
+            greater = current;
         } else {
             greater = greater.next;
         }
@@ -33,14 +34,3 @@ let nextLargerNodes = function(head) {
 
     return output
 };
-
-class ListNode {
-    constructor(val, next) {
-        this.val = (val === undefined ? 0 : val);
-        this.next = (next === undefined ? null : next);
-    }
-}
-
-const head = new ListNode(2, new ListNode(1, new ListNode(5)));
-
-console.log(nextLargerNodes(head))
